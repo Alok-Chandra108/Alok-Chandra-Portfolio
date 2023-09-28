0924a25e09efde34ae6266dce907f3f0598d0b05
alok-chandra108 <alokchandra2611@gmail.com>
2023-09-28 16:07:43 +0530

    Completed Portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menulinks");
+    const icon = document.querySelector(".ham-icon");
+    const window = document.querySelector("");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
